2ccc3030a6099fd466dfbe89108087249cd83f5d
Inwoo <lys91892@gmail.com>
2023-10-26 23:22:18 +0900

    4주차 timer 제작

---
+++ app/src/main/java/com/yongsu/floproject/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.yongsu.floproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
--- app/src/main/java/com/yongsu/floproject/TestActivity.kt
+++ app/src/main/java/com/yongsu/floproject/TestActivity.kt
@@ -2,7 +2,11 @@ package com.yongsu.floproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.yongsu.floproject.databinding.ActivitySongBinding
 import com.yongsu.floproject.databinding.ActivityTestBinding
 
@@ -10,36 +14,88 @@ class TestActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityTestBinding
 
+    private var nowTime:String = ""
+    private var minute = 0
+    private var second = 0
+    private var started = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 객체를 만들어줌
-        val a = A()
-        val b = B()
-        a.start()
-        // join()을 통하여 다른 쓰레드가 실행되지 못하게 막음
-        a.join()
-        b.start()
+        val handler = Handler(Looper.getMainLooper())
+        initTimeStart(handler)
     }
 
-    // 새로운 클래스를 만들고 Thread를 상속받음
-    class A : Thread(){
-        // run을 override 하고 내부의 코드가 Thread가 시작함과 동시에 실행됨.
-        override fun run(){
-            super.run()
-            for(i in 1..1000){
-                Log.d("testtest", "first : $i")
+    private fun initTimeStart(handler: Handler){
+        with(binding){
+            startBtn.setOnClickListener {
+
+
+                val inputTime = mainTimeEt.text.toString().split(":").map{it.toInt()}
+                minute = inputTime[0]
+                second = inputTime[1]
+                started = true
+
+                initStartPause()
+
+                Thread{
+                    while(started){
+                        Thread.sleep(1000)
+                        if(!started) break
+
+                        if(second == 0 && minute == 0){
+                            started = false
+
+                            handler.post{
+                                initStartPause()
+                                mainTimeEt.setText("00:00")
+                            }
+
+                            break
+                        }
+
+                        if(second == 0 && minute>0){
+                            minute -= 1
+                            second = 59
+                        } else if(second > 0){
+                            second -= 1
+                        }
+                        var finalMinute = minute
+                        val finalSecond = second
+
+
+                        handler.post{
+                            mainTimeEt.setText("${finalMinute}:${finalSecond}")
+                        }
+                    }
+                }.start()
+            }
+
+            pauseBtn.setOnClickListener {
+                started = false
+                initStartPause()
+            }
+
+            resetBtn.setOnClickListener {
+                started = false
+                initStartPause()
+
+                mainTimeEt.setText("00:00")
             }
         }
     }
 
-    class B : Thread(){
-        override fun run(){
-            super.run()
-            for(i in 1000 downTo 1){
-                Log.d("testtest", "second : $i")
+    private fun initStartPause(){
+        with(binding){
+            if(started){
+                startBtn.visibility = View.GONE
+                pauseBtn.visibility = View.VISIBLE
+            }else{
+                startBtn.visibility = View.VISIBLE
+                pauseBtn.visibility = View.GONE
             }
         }
     }
