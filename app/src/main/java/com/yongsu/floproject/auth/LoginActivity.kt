7b8f95eac3c4e58d87172dccc0858514450c54c6
Inwoo <lys91892@gmail.com>
2023-11-23 14:09:06 +0900

    8주차 로그인

--- app/src/main/java/com/yongsu/floproject/auth/LoginActivity.kt
+++ app/src/main/java/com/yongsu/floproject/auth/LoginActivity.kt
@@ -3,7 +3,11 @@ package com.yongsu.floproject.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.yongsu.floproject.MainActivity
 import com.yongsu.floproject.databinding.ActivityLoginBinding
+import com.yongsu.floproject.roomdb.database.SongDatabase
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,5 +23,49 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
 
+        binding.loginSignInBtn.setOnClickListener {
+            login()
+        }
+    }
+
+    // Validation Check
+    private fun login(){
+        if(binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this@LoginActivity, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(binding.loginPasswordEt.text.toString().isEmpty()){
+            Toast.makeText(this@LoginActivity, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val email : String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val pwd : String = binding.loginPasswordEt.text.toString()
+
+        val songDB = SongDatabase.getInstance(this@LoginActivity)!!
+        val user = songDB.userDao().getUser(email, pwd)
+
+        user?.let{
+            Log.d("LOGIN_ACT/GET_USER", "userId : ${user.id}, $user")
+            saveJwt(user.id)
+            startMainActivity()
+        }
+
+        Toast.makeText(this@LoginActivity, "회원 정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    // user의 id값을 jwt토큰으로 대체해서 사용
+    // 따라서 로그인을 진행하기 전에 sharedPreference로 id를 저장
+    private fun saveJwt(jwt: Int){
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
+        val editor = spf.edit()
+
+        editor.putInt("jwt", jwt)
+        editor.apply()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/yongsu/floproject/fragment/LockerFragment.kt
+++ app/src/main/java/com/yongsu/floproject/fragment/LockerFragment.kt
@@ -2,11 +2,14 @@ package com.yongsu.floproject.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
+import com.yongsu.floproject.MainActivity
 import com.yongsu.floproject.adapter.LockerVPAdapter
 import com.yongsu.floproject.auth.LoginActivity
 import com.yongsu.floproject.databinding.FragmentLockerBinding
@@ -26,9 +29,7 @@ class LockerFragment : Fragment() {
 
         initViewPager()
 
-        binding.lockerLoginTv.setOnClickListener {
-            startActivity(Intent(activity, LoginActivity::class.java))
-        }
+
 
         return binding.root
     }
@@ -42,8 +43,44 @@ class LockerFragment : Fragment() {
                 tab.text = information[position]
             }.attach()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initViews()
+    }
+
+    private fun getJwt():Int{
+        // activity?를 붙이는 이유는 프래그먼트에서 사용할 때의 방법이기 떄문
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        return spf!!.getInt("jwt", 0)
+    }
 
 
+    private fun initViews(){
+        val jwt: Int = getJwt()
+        Log.d("jwtjwt", "$jwt")
+        if(jwt == 0){
+            binding.lockerLoginTv.text = "로그인"
+            binding.lockerLoginTv.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }else{
+            binding.lockerLoginTv.text = "로그아웃"
+            binding.lockerLoginTv.setOnClickListener {
+                // 로그아웃 진행
+                logout()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun logout() {
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        // jwt라는 키에 해당하는 값을 없앰
+        editor.remove("jwt")
+        editor.apply()
     }
 
 }
